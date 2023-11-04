55ed4e02dadd6a49b7542d99ce6fd8e0ae2e0ff6
Shashi-0706 <1ms21cy046@msrit.edu>
2023-11-04 16:42:07 +0530

    Login,Signup Page Added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-
+import Navbar from "./components/Navbar/navbar";
+import Home from "./screens/Home/Home";
+import Footer from "./components/Footer/footer";
+import Login from "./components/Login/login";
+import Signup from "./components/Signup/signup";
 const App = () => (
   <>
     <Navbar />
     <Routes>
       <Route path="/" element={<Home />} />
-      {/* <Route path="/login" element={<Login />} /> */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
       {/* <Route path="/register" element={<Register />} /> */}
       {/* <Route path="*" element={<NotFound />} /> */}
     </Routes>
