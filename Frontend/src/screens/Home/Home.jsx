75bb64adb4c857c32cb9806eeeb2d0c056d5e2c8
Shashi-0706 <1ms21cy046@msrit.edu>
2023-11-05 01:11:24 +0530

    Dashboard updated

--- Frontend/src/screens/Home/Home.jsx
+++ Frontend/src/screens/Home/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import './Home.css'; // Create a CSS file for styling
+import './Home.css'; // Create a CSS file for additional styling if needed
+import bg from "../../assets/bg.avif";
 
 function Home() {
   return (
-    <div className="home-container">
-      <h1 className="text-4xl font-bold">Welcome to HealthWallet</h1>
-      <p className="text-lg mt-4">Your trusted health management platform.</p>
+    <div className="flex justify-center items-center h-screen">
+      <div className="md:w-1/2 p-8 text-black">
+        <h1 className="text-[50px] font-bold">Welcome to <span className=' text-blue-500'>HealthWallet</span></h1>
+        <p className="text-[25px] mt-4 ">Your trusted health management platform.</p>
+      </div>
+      <div className="md:w-1/2">
+        <img src={bg} alt="Background" className="w-full h-auto" />
+      </div>
     </div>
   );
 }
